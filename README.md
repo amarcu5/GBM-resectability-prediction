3690b47028309bc8cac9a461786b1c5b50923ac3
amarcu5 <adampetermarcus@gmail.com>
2018-11-09 14:53:37 +0000

    First commit to GitHub Pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# GBM-resectability-prediction
---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'gbmResectabilityCache';
+const filesToCache = [
+  './calc.html',
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
